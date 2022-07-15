74dc2cd8c498ae15f7e669adb10f90cccfef1d7d
dom <test@gmail.com>
2022-07-15 21:22:27 +0700

    dockerise

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-sudo docker-compose up -d
-mvn spring-boot:run
+docker run -p 3306:3306 --name=docker-mysql --env="MYSQL_ROOT_PASSWORD=supersecret" --env="MYSQL_PASSWORD=supersecret" --env="MYSQL_DATABASE=food_app" mysql
+docker build -f Dockerfile -t food .
+docker run -t --name food --link docker-mysql:mysql -p 8080:8080 food
