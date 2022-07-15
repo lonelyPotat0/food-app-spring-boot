78974d92de7f351bbf413424dfc9e18a01ccf83f
dom <test@gmail.com>
2022-07-15 21:31:03 +0700

    dockerise

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 docker run -p 3306:3306 --name=docker-mysql --env="MYSQL_ROOT_PASSWORD=supersecret" --env="MYSQL_PASSWORD=supersecret" --env="MYSQL_DATABASE=food_app" mysql
-docker build -f Dockerfile -t food .
+docker build -f DockerFile -t food .
 docker run -t --name food --link docker-mysql:mysql -p 8080:8080 food
